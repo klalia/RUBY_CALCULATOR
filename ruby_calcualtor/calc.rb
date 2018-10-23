b64b1767fa8edf66f9468a3192c69752d64728d5
Keerat <klalia@spartaglobal.com>
2018-10-23 09:56:15 +0100

    Calculator with basic and advanced option but no sqrt

---
+++ ruby_calcualtor/calc.rb
@@ -0,0 +1,80 @@
+puts "Which calculator? (1 for basic, 2 for advanced)"
+$stdout.flush #
+val = gets.to_i
+if val== 1
+puts "Enter a number"
+$stdout.flush #
+val1 = gets.to_i
+puts "+ or - or * or /"
+$stdout.flush #
+sym = gets.chomp
+puts "Enter the next number"
+$stdout.flush #
+val2 = gets.to_i
+
+def addition val1, val2
+  return val1+val2
+end
+
+add = addition val1, val2
+
+def subtraction val1, val2
+  return val1-val2
+end
+
+sub = subtraction val1, val2
+
+def division val1, val2
+  return val1/val2
+end
+
+div = division val1, val2
+
+def multiplication val1, val2
+  return val1*val2
+end
+
+mult = multiplication val1, val2
+
+
+if sym == "+"
+puts add
+elsif sym == "-"
+puts sub
+elsif sym == "/"
+puts div
+elsif sym == "*"
+puts mult
+else puts "error"
+end
+end
+
+if val == 2
+puts "Enter a number"
+$stdout.flush #
+val1 = gets.to_i
+puts "** or sqrt"
+$stdout.flush #
+sym = gets.chomp
+puts "Enter the next number"
+$stdout.flush #
+val2 = gets.to_i
+
+def power num1,num2
+  return num1**num2
+end
+
+pow = power val1, val2
+
+def sqrt num1
+  return sqrt num1
+end
+squreroot = sqrt val1
+
+if sym == "**"
+  puts pow
+end
+if sym == "sqrt"
+  puts squreroot
+end
+end
